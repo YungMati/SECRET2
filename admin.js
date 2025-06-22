61dd6ee97b7fcab99b976ba8d89df0d4f83d2d66
YungMati <pikxi0505@gmail.com>
2025-06-22 13:58:54 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,33 @@
+const username = "YungMati";
+const password = "vawjej-fetfuq-gIqwy5";
 
+function login() {
+  const u = document.getElementById("username").value;
+  const p = document.getElementById("password").value;
+
+  if (u === username && p === password) {
+    document.getElementById("loginScreen").style.display = "none";
+    document.getElementById("adminPanel").style.display = "block";
+  } else {
+    alert("Wrong credentials");
+  }
+}
+
+function addProduct() {
+  const data = {
+    name: document.getElementById("name").value,
+    image: document.getElementById("image").value,
+    price: parseFloat(document.getElementById("price").value),
+    description: document.getElementById("description").value,
+    batch: document.getElementById("batch").value,
+    category: document.getElementById("category").value
+  };
+
+  fetch('/add-product', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  }).then(res => res.json()).then(resp => {
+    alert("Product added!");
+  });
+}
